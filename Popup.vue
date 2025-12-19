9e88a57574d286c15c64702248dc5de65fe8e81f
serii <seriiburduja@gmail.com>
2025-12-19 07:06:16 +0200

    feat: popup component

---
+++ Popup.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const emits = defineEmits(["emit_close"]);
+const props = defineProps({
+  title: { type: String, required: false, default: "Popup" },
+});
+</script>
+
+<template>
+  <div class="fixed inset-0 z-50 flex items-center justify-center">
+    <div
+      class="absolute inset-0 bg-slate-800/70 backdrop-blur-sm transition"
+      @click="emits('emit_close')"
+    ></div>
+    <div class="relative z-10 w-96 rounded-lg bg-slate-800 p-6 shadow-lg">
+      <header
+        class="mb-4 flex items-center justify-between border-b border-slate-700 pb-2"
+      >
+        <h2 class="text-xl font-semibold">{{ title }}</h2>
+        <font-awesome-icon
+          @click="$emit('emit_close')"
+          class="trasition-color cursor-pointer text-slate-400 hover:text-white"
+          icon="fa-solid fa-square-xmark"
+        />
+      </header>
+      <slot />
+    </div>
+  </div>
+</template>
