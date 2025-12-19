49780aafdd28586b494b9babf85a5e046460e754
serii <seriiburduja@gmail.com>
2025-12-19 07:29:27 +0200

    feat: popup width

--- Popup.vue
+++ Popup.vue
@@ -2,16 +2,19 @@
 const emits = defineEmits(["emit_close"]);
 const props = defineProps({
   title: { type: String, required: false, default: "Popup" },
+  width: { type: String, required: false, default: "600px" },
 });
 </script>
 
 <template>
   <div class="fixed inset-0 z-50 flex items-center justify-center">
     <div
-      class="absolute inset-0 bg-slate-800/70 backdrop-blur-sm transition"
+      class="absolute inset-0 bg-slate-900/70 backdrop-blur-sm transition"
       @click="emits('emit_close')"
     ></div>
-    <div class="relative z-10 w-96 rounded-lg bg-slate-800 p-6 shadow-lg">
+    <div
+      :class="`relative z-10 w-[${width}] rounded-lg bg-slate-800 p-6 shadow-lg`"
+    >
       <header
         class="mb-4 flex items-center justify-between border-b border-slate-700 pb-2"
       >
